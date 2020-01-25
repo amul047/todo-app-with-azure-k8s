67488661ac920c1c2121f181c9e5246a3959e450
VOCUS\Aamir.Mulla <aamir.mulla@vocusgroup.co.nz>
2020-01-26 11:23:42 +1300

    Deploy linux docker container with dot net core 3 api into cluster using terraform

--- deploy/terraform/Infrasturcture Template.tf
+++ deploy/terraform/Infrasturcture Template.tf
@@ -1,22 +1,16 @@
 provider "azurerm" {
 }
-resource "azurerm_resource_group" "rg" {
-        name = "todoResourceGroup"
+resource "azurerm_resource_group" "tdrg" {
+        name = "todoresourcegroup"
         location = "australiaeast"
 }
 
-resource "azurerm_kubernetes_cluster" "example" {
-  name                = "todoKubernetesCluster"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "todoKubernetesCluster"
+resource "azurerm_kubernetes_cluster" "tdkc" {
+  name                = "todokubernetescluster"
+  location            = azurerm_resource_group.tdrg.location
+  resource_group_name = azurerm_resource_group.tdrg.name
+  dns_prefix          = "todokubernetescluster"
 
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2_v2"
-  }
-  
   addon_profile {
     kube_dashboard {
       enabled = true
@@ -31,12 +25,62 @@ resource "azurerm_kubernetes_cluster" "example" {
   tags = {
     Environment = "Production"
   }
+  
+  default_node_pool {
+    vm_size    = "Standard_D2_v2"
+    name       = "tduaprofile"
+	node_count = 1
+  }
 }
 
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+  value = azurerm_kubernetes_cluster.tdkc.kube_config.0.client_certificate
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config_raw
+  value = azurerm_kubernetes_cluster.tdkc.kube_config_raw
+}
+
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.tdkc.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.tdkc.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.tdkc.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.tdkc.kube_config.0.cluster_ca_certificate)
+}
+
+resource "kubernetes_pod" "tduap" {
+  metadata {
+    name = "todoupdatesapipod"
+    labels = {
+      App = "toDoUpdatesApi"
+    }
+  }
+
+  spec {
+    container {
+      image = "amul047/updatesapi"
+      name  = "todoupdatesapicontainer"
+	  
+      port {
+        container_port = 80
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "tduas" {
+  metadata {
+    name = "todoupdatesapiservice"
+  }
+  spec {
+    selector = {
+      App = kubernetes_pod.tduap.metadata[0].labels.App
+    }
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
 }
