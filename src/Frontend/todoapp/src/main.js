399afa8be01a10eb7fd5e1a5fcdc9367bd7f796e
VOCUS\Aamir.Mulla <aamir.mulla@vocusgroup.co.nz>
2020-01-30 20:36:50 +1300

    Add vue bootstrap

--- src/Frontend/todoapp/src/main.js
+++ src/Frontend/todoapp/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, LinkPlugin} from 'bootstrap-vue'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(LinkPlugin)
 
 new Vue({
   render: h => h(App),
