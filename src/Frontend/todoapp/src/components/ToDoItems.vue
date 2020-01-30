5ea5176f78936ff387fc808520407b3a2a0f5a36
VOCUS\Aamir.Mulla <aamir.mulla@vocusgroup.co.nz>
2020-01-30 21:50:48 +1300

    page routing

---
+++ src/Frontend/todoapp/src/components/ToDoItems.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <b-jumbotron>
+      <h1>To do items</h1>
+    </b-jumbotron>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDoItems',
+  props: {
+    msg: String
+  }
+}
+</script>
--- src/Frontend/todoapp/src/main.js
+++ src/Frontend/todoapp/src/main.js
@@ -1,13 +1,26 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
 import { BootstrapVue, LinkPlugin} from 'bootstrap-vue'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import '../node_modules/bootstrap-vue/dist/bootstrap-vue.css'
+import ToDoItems from './components/ToDoItems.vue'
+import App from './App.vue'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(LinkPlugin)
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: ToDoItems },
+  { path: '/todoitems', component: ToDoItems }
+]
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
